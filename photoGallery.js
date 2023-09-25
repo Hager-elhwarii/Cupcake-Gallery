996db8f060d731337bdefaf7e5714f7a9edd48fc
hager-dot <hagerelhwarii6633@gmail.com>
2023-09-25 10:10:25 +0300

    :sparkles: Update cupcake images

---
+++ photoGallery.js
@@ -0,0 +1,10 @@
+
+function fadeOut(img) {
+ img.style.opacity = 0.3;
+ img.style.transition = "0.6s ease-in-out";
+}
+function fadeIn(img) {
+    img.style.opacity = 1;
+    img.style.transition = "0.6s ease-in-out";
+}
+
